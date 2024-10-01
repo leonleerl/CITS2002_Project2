c8fed9c0fd638b6cfe70f4aba3e03f7197d89a71
leonleerl <leon.runlongli@gmail.com>
2024-10-01 09:33:12 +0800

    initial commit

---
+++ simulation.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define PROCESS_NUM 4
+
+typedef struct
+{
+    int process_id;
+    int page_num;
+    int last_accessed;
+} memory;
+
+// 参数1:in.txt 参数2:out.txt
+int main(int argc, char *argv[])
+{
+
+    return 0;
+}
